8805d10e8ee76692d92f626567df0fef71aefb06
KSY <kisy324@naver.com>
2021-07-17 01:07:31 +0900

    유저프로필, 프렌드 초기 세팅

--- src/main/java/com/facebook/facebookclone/controller/UserProfileController.java
+++ src/main/java/com/facebook/facebookclone/controller/UserProfileController.java
@@ -1,9 +1,14 @@
 package com.facebook.facebookclone.controller;
 
+import com.facebook.facebookclone.repository.UserProfileRepository;
+import com.facebook.facebookclone.service.UserProfileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 public class UserProfileController {
+
+    private final UserProfileService userProfileService;
+    private final UserProfileRepository userProfileRepository;
 }
